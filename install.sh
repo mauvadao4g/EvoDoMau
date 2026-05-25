8c0ebefa2ca0a7a9964f6148d350f9ff609776bb
Mauvadao4g <mauvadao4g@gmail.com>
2026-05-25 12:18:21 -0300

    Update Mon May 25 12:18:21 -03 2026

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# BY:  MAUVADAO
+# VER:  $
+
+
+
+EVO="https://raw.githubusercontent.com/mauvadao4g/EvoDoMau/refs/heads/main/evoInstall.sh"
+
+SSL="https://raw.githubusercontent.com/mauvadao4g/EvoDoMau/refs/heads/main/setup_ssl.sh"
+
+
+
+echo "1 => EVOLUTION API"
+echo "2 => SSL CERTIFICADO"
+echo "0 => SAIR"
+echo "..."
+read -p "Deseja instalar qual serviço: " xd
+
+case  "$xd" in
+1) bash <(wget -qO- $EVO); exit 0;;
+2) bash <(wget -qO- $SSL); exit 0;;
+0)
+echo "Saindo ..."
+sleep 2
+exit 0
+
+esac
